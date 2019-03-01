a7c9f1861eabdc5fc4533294a61109366131ef67
IAKOVKIR <37230942+IAKOVKIR@users.noreply.github.com>
2019-03-01 19:09:42 +1100

    2 pages for registration added

--- app/src/main/java/com/example/semicolon/Login.kt
+++ app/src/main/java/com/example/semicolon/Login.kt
@@ -55,7 +55,7 @@ class Login : Activity() {
 
         //"Sign Up" listener
         createBut.setOnClickListener {
-            val intent = Intent(this, FirstRegoList::class.java)
+            val intent = Intent(this, Main2Activity::class.java)
             startActivity(intent)
         }
 
