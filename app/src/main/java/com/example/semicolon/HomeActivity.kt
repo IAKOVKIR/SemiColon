6673581e61d93bbd1f119c13435978125462482d
IAKOVKIR <37230942+IAKOVKIR@users.noreply.github.com>
2019-03-07 19:54:49 +1100

    comments for HomeActivity.kt were added

--- app/src/main/java/com/example/semicolon/HomeActivity.kt
+++ app/src/main/java/com/example/semicolon/HomeActivity.kt
@@ -4,37 +4,45 @@ import android.os.Bundle
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.view.View
 import android.widget.TextView
 
 class HomeActivity : Activity() {
 
     private var user: String? = null
     private var log = Login()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        
         //get username sent from the log in activity
         val intent = intent
         val b = intent.extras
         user = b!!.getString("user")
     }
 
+    /**
+     * @function onStart() displays text on TextView
+     */
+
     public override fun onStart() {
         super.onStart()
         val view = findViewById<TextView>(R.id.UserName)
-        view.text = "Welcome " + user!!
+        view.text = "welcome $user"
     }
 
+    /**
+     * @function logOut() removes saved username and password from SharedPreferences and
+     * returns to login form
+     */
 
-    fun logOut(view: View) {
-        val sharedPrefs = getSharedPreferences(log.prefName
-            , Context.MODE_PRIVATE)
+    fun logOut() {
+        val sharedPrefs = getSharedPreferences(log.prefName, Context.MODE_PRIVATE)
         val editor = sharedPrefs.edit()
         editor.clear()
         editor.apply()
         user = ""
-        //show login form
+
         val intent = Intent(this, Login::class.java)
         startActivity(intent)
         finish()
