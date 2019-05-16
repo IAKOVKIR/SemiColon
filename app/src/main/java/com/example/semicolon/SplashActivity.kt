66ed4d51a5c138c643709fe40cb93e305952630b
IAKOVKIR <37230942+IAKOVKIR@users.noreply.github.com>
2019-05-16 13:04:56 +1000

    splash screen was updated

--- app/src/main/java/com/example/semicolon/SplashActivity.kt
+++ app/src/main/java/com/example/semicolon/SplashActivity.kt
@@ -3,25 +3,30 @@ package com.example.semicolon
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 
 import java.util.Timer
 import java.util.TimerTask
 
-class WelcomeLabel : Activity() {
+class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome_label)
+        setContentView(R.layout.activity_splash)
 
         val timer = Timer()
         val intent = Intent(this, Login::class.java)
+        val animFadeIn = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in)
+        val textSemiColon = findViewById<TextView>(R.id.label)
+        textSemiColon.startAnimation(animFadeIn)
 
         timer.schedule(object : TimerTask() {
             override fun run() {
                 startActivity(intent)
                 finish()
             }
-        },  1400)
+        },  1000)
 
     }
 }
